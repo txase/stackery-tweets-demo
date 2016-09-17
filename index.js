8bb18cd43bbe6986e6fcdeceb5ecffd6b8730d4d
Chase Douglas <chase@immun.io>
2016-09-17 14:07:32 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const Twitter = require('twitter'),
+      secrets = { 
+        consumer_key: StackeryContext.deployment.config.consumerKey,
+        consumer_secret: StackeryContext.deployment.config.consumerSecret,
+        bearer_token: StackeryContext.deployment.config.bearerToken
+      },  
+      client = new Twitter(secrets)
+
+module.exports = function handler(message) {
+  return new Promise((resolve, reject) => {
+    client.get('statuses/user_timeline',
+               {screen_name: 'txase'},
+               (err, tweets) => {
+      if (err)
+        resolve(err)
+      else
+        reject({body: tweets})
+    })  
+  })  
+}
