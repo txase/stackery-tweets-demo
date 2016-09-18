58fdbdfed92c007d135a448cfa0107517e37504b
Chase Douglas <chase@immun.io>
2016-09-18 10:50:32 -0700

    Switch to inline code
    index.js is now just a copy for reference.

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
-'use strict'
+function handler(message, output) {
+  var request = {
+    url: `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${encodeURIComponent(message.resourceParams.handle)}`,
+    headers: {
+      Authorization: `Bearer ${StackeryContext.deployment.config.bearerToken}`
+    }
+  }
 
-const Twitter = require('twitter'),
-      secrets = { 
-        consumer_key: StackeryContext.deployment.config.consumerKey,
-        consumer_secret: StackeryContext.deployment.config.consumerSecret,
-        bearer_token: StackeryContext.deployment.config.bearerToken
-      },  
-      client = new Twitter(secrets)
-
-module.exports = function handler(message) {
-  return new Promise((resolve, reject) => {
-    client.get('statuses/user_timeline',
-               {screen_name: message.resourceParams.handle},
-               (err, tweets) => {
-      if (err) {
-        reject(err)
-        return
+  return output(request, {waitForResponses: true})
+    .then((responses) => {
+      return {
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: responses[0].body
       }
-
-      let response = {
-            headers: {
-              'Access-Control-Allow-Origin': '*'
-            },
-            body: tweets
-          }
-
-      resolve(response)
-    })  
-  })  
+    })
 }
